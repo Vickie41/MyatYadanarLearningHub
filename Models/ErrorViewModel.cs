103560109872796db6c3ce8ce09a05f9111ea235
Vickie41 <vickieforstudy@gmail.com>
2025-09-08 14:29:37 +0630

    Initial commit with .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyatYadanarLearningHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
